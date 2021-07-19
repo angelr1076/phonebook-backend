e13707d7241ba93f098569fdd25e67121c777e00
Angel Rodriguez <angelr1076@gmail.com>
2021-07-19 08:23:25 -0400

    Added rebuild of frontend and pushed to Heroku

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const Person = require("./models/person");
 const morgan = require("morgan");
 const cors = require("cors");
+const Person = require("./models/person");
 
+app.use(express.static("build"));
 app.use(cors());
 app.use(express.json());
-app.use(express.static("build"));
 
 morgan.token("body", (req) =>
   req.method === "POST" ? JSON.stringify(req.body) : null,
@@ -16,31 +16,8 @@ app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body"),
 );
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
 app.get("/", (request, response) => {
-  response.send("<h1>Hello World!</h1>");
+  response.send("<h1><u>Welcome to the phonebook app...</u></h1>");
 });
 
 app.get("/api/persons", (request, response) => {
@@ -49,10 +26,16 @@ app.get("/api/persons", (request, response) => {
   });
 });
 
-app.get("/api/persons/:id", (request, response) => {
-  Person.findById(request.params.id).then((person) => {
-    response.json(person);
-  });
+app.get("/api/persons/:id", (request, response, next) => {
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
 app.get("/info", (request, response) => {
@@ -63,11 +46,12 @@ app.get("/info", (request, response) => {
   );
 });
 
-app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  persons = persons.filter((person) => person.id !== id);
-
-  response.status(204).end();
+app.delete("/api/persons/:id", (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
 });
 
 app.post("/api/persons", (request, response) => {
@@ -79,18 +63,6 @@ app.post("/api/persons", (request, response) => {
     });
   }
 
-  // For section 3.14 we will allow multiple entries per person
-  // const existingPerson = persons.find(
-  //   (person) => person.name.toLowerCase() === body.name.toLowerCase(),
-  // );
-
-  // if (existingPerson) {
-  //   response.status(400).json({
-  //     error: "Name must be unique",
-  //   });
-  //   return false;
-  // }
-
   const person = new Person({
     name: body.name,
     number: body.number,
@@ -100,6 +72,42 @@ app.post("/api/persons", (request, response) => {
     response.json(savedPerson);
   });
 });
+
+app.put("/api/persons/:id", (request, response, next) => {
+  const body = request.body;
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  };
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then((updatedPerson) => {
+      response.json(updatedPerson);
+    })
+    .catch((error) => next(error));
+});
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+// handler of requests with unknown endpoint
+app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response
+      .status(400)
+      .send({ error: "The id entered was improperly formatted" });
+  }
+
+  next(error);
+};
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
